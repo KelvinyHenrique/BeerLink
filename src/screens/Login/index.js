b8062dfa57b5d094d0760c4f72882742dda23495
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-29 11:20:39 -0300

    Building Sign in

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './styles';
-import { View } from 'react-native';
+import { View, Animated, Dimensions, Easing, Text } from 'react-native';
+
+const screenHeight = Math.round(Dimensions.get('window').height);
+
+const thirty = (20 * screenHeight) / 100;
 
 
 function Login() {
+
+    const fadeAnim = useRef(new Animated.Value(screenHeight)).current;
+
+    const fadeIn = () => {
+        // Will change fadeAnim value to 1 in 5 seconds
+        Animated.timing(fadeAnim, {
+            toValue: thirty,
+            duration: 1500,
+            easing: Easing.bounce,
+            useNativeDriver: false
+        }).start();
+    };
+    useEffect(() => {
+        fadeIn();
+    }, []);
+
+
     return(
         <View style={styles.container}>
-            
+        <Animated.View style={[styles.BoxHeader, { height: fadeAnim }]}>
+        <Text>Entrar</Text>
+        </Animated.View>
+        <View style={styles.boxContainerBottom}>
+
+        </View>
         </View>
     );
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -6,6 +6,18 @@ export default StyleSheet.create({
     container:{
         width:'100%',
         height:'100%',
+        backgroundColor:'#FFF',
         backgroundColor:'#BA0C2F',
+    },
+    BoxHeader:{
+        width:'100%',
+        backgroundColor:'#BA0C2F',
+    },
+    boxContainerBottom:{
+        width:'100%',
+        height:'80%',
+        backgroundColor:'#FFF',
+        borderTopLeftRadius:33,
+        borderTopRightRadius:33,    
     },
 });

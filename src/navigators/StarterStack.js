da0500ed75a6ff0159ae72c947f839537cd9c00a
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-30 10:57:04 -0300

    Building Home

--- src/navigators/StarterStack.js
+++ src/navigators/StarterStack.js
@@ -3,32 +3,42 @@ import { createStackNavigator } from '@react-navigation/stack';
 import FirstScreen from '../screens/FirstScreen';
 import Login from '../screens/Login';
 import SignIn from '../screens/SignIn';
+import Home from '../screens/Home';
 
 const Stack = createStackNavigator();
 
 export default function TransationsStack() {
 
-    return( 
-        <Stack.Navigator>
-        <Stack.Screen name="FirstScreen" component={FirstScreen}  options={{
-          headerShown:false,
-        }}/>
-        <Stack.Screen name="Login" component={Login}  options={{
-            title:false,
-          headerShown:true,
-          headerStyle: {
-              backgroundColor:'#BA0C2F'
-          },
-          headerTintColor:'#fff'
-        }}/>
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="FirstScreen" component={FirstScreen} options={{
+        headerShown: false,
+      }} />
+      <Stack.Screen name="Login" component={Login} options={{
+        title: false,
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#BA0C2F'
+        },
+        headerTintColor: '#fff'
+      }} />
       <Stack.Screen name="SignIn" component={SignIn} options={{
-            title:false,
-          headerShown:true,
-          headerStyle: {
-              backgroundColor:'#BA0C2F'
-          },
-          headerTintColor:'#fff'
-          }}/>
-      </Stack.Navigator>
-    );
+        title: false,
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#BA0C2F'
+        },
+        headerTintColor: '#fff'
+      }} />
+
+      <Stack.Screen name="Home" component={Home} options={{
+        title: false,
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#BA0C2F'
+        },
+        headerTintColor: '#fff'
+      }} />
+    </Stack.Navigator>
+  );
 }
---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React, { useRef, useEffect } from 'react';
+
+import { View, Animated, Dimensions, Easing, Text, TouchableOpacity } from 'react-native';
+import { TextInput, TouchableHighlight } from 'react-native-gesture-handler';
+import styles from './styles';
+
+function Home() {
+
+    return (
+        <View style={styles.container}>
+          
+        </View>
+    );
+}   
+
+export default Home;

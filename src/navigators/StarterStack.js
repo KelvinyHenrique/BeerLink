4a67a9e5733a7fafe424f9f59d34a5472d9b5aea
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-02 19:27:18 -0300

    Bugfix

--- src/navigators/StarterStack.js
+++ src/navigators/StarterStack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import FirstScreen from '../screens/FirstScreen';
 import VirtualBar from '../screens/VirtualBar';
 import Login from '../screens/Login';
@@ -12,52 +12,49 @@ import Mapa from '../screens/Map';
 const Stack = createStackNavigator();
 
 export default function TransationsStack() {
+
   return (
-       <Stack.Navigator>
-         <Stack.Screen name="VirtualBar" component={VirtualBar}  options={{
-          headerShown:false,
-        }}/>  
-    <Stack.Navigator>
-      <Stack.Screen
-        name="QrScanner"
-        component={Mapa}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{
-          title: false,
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: '#BA0C2F',
-          },
-          headerTintColor: '#fff',
-        }}
-      />
-      <Stack.Screen
-        name="SignIn"
-        component={SignIn}
-        options={{
-          title: false,
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: '#BA0C2F',
-          },
-          headerTintColor: '#fff',
-        }}
-      />
-    
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          title: false,
-          headerShown: false,
-        }}
-      />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="QrScanner"
+          component={Mapa}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: false,
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: '#BA0C2F',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{
+            title: false,
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: '#BA0C2F',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: false,
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
   );
 }

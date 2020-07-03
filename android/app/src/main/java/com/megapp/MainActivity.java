bb20b5aaaca685280805c74aaa2aca737c49a15b
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-03 12:20:02 -0300

    Corrigindo nome do app

--- android/app/src/main/java/com/megapp/MainActivity.java
+++ android/app/src/main/java/com/megapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.megapp;
+package com.beerlink;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "megapp";
+    return "beerlink";
   }
 }
